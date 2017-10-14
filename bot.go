f77d4f15010ac70ea1307161f99beb8f11cb74a7
Colin <colin.adler@yahoo.com>
2017-10-14 02:08:19 -0400

    ignore messages from bots

--- bot.go
+++ bot.go
@@ -82,7 +82,7 @@ func (b *Bot) listen(service Service, messageChan <-chan Message) {
 	serviceName := service.Name()
 	for {
 		message := <-messageChan
-		if message.IsExcluded() {
+		if message.IsExcluded() || message.IsBot() {
 			continue
 		}
 		plugins := b.Services[serviceName].Plugins
